6654d909c8e166bea76a3d89e58ac52642e7b36a
prakshh <prakashdas18@gmail.com>
2025-04-08 01:00:17 +0530

    updated deployed backend url

--- anonymous-chat-app/client/src/socket.js
+++ anonymous-chat-app/client/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // change to your deployed backend later
+// const socket = io("http://localhost:5000"); // change to your deployed backend later
+const socket = io("https://hushroom.onrender.com"); 
 
 export default socket;
